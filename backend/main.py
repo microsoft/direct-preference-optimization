868084630a1ec8ede0ce4dedb02258fbbd893dd4
Pete Messina <petermessina@microsoft.com>
2024-05-17 11:49:24 -0400

    Migrating to fastapi and fixing linting issues

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,33 @@
 import os
 import yaml
 
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
 from dotenv import load_dotenv
 
-load_dotenv(override=True, dotenv_path=f"{os.getcwd()}/backend/.env")
+load_dotenv(override=True, dotenv_path=f"{os.getcwd()}/.env")
 
-from approaches.chatconversation import ChatConversation
+from models.chat_request import Chat
+from approaches.chat_conversation import ChatConversation
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    chat_config = yaml.safe_load(open("backend/chat_config.yaml", "r"))
-    message = request.json.get("dialog")
+@app.post("/chat")
+def chat(chat_message: Chat):
+    """API endpoint for chat conversation."""
+    chat_config = yaml.safe_load(open("chat_config.yaml", "r"))
     conversation = ChatConversation(
         azure_openai_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
         azure_openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
         azure_search_endpoint=os.environ["AZURE_SEARCH_ENDPOINT"],
         azure_search_key=os.environ["AZURE_AI_SEARCH_API_KEY"]
     )
-    
+
     response = conversation.chat(
         chat_config=chat_config,
-        prompt=message
+        prompt=chat_message.dialog
     )
 
-    return jsonify(response.to_item())
+    return response.to_item()
 
 if __name__ == '__main__':
     app.run(debug=True)
